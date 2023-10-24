165c9d83d1c64f54b7c3dc18c67ec284dfccc24c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2023-10-24 15:33:43 -0500

    Update oled_pot.cpp

--- oled_pot.cpp
+++ oled_pot.cpp
@@ -9,16 +9,6 @@
 #define ALTO_PANTALLA 64
 // Variables globales
 Adafruit_SSD1306 pantalla(ANCHO_PANTALLA, ALTO_PANTALLA, &Wire, -1);
-uint16_t potentiometerPin = 34;
-uint32_t tiempo_actual = 0;
-const int sampleRate = 200;  // Tasa de muestreo en milisegundos
-const int graphWidth = 105; // Ancho de la gráfica en píxeles
-const int graphHeight = 43; // Altura de la gráfica en píxeles
-const int Yinit = 16;
-static int x = 10;
-static int prevX , prevY = 0;
-int y=0;
-int sensorValue;
 
 // Funciones
 void setup()
@@ -29,7 +19,7 @@ void setup()
   pantalla.clearDisplay();
   pantalla.setTextColor(SSD1306_WHITE);
   pantalla.setTextSize(1);
-  pantalla.drawPixel(x, y, SSD1306_WHITE);
+  pantalla.drawPixel(22, 30, SSD1306_WHITE);
   pantalla.drawLine(5, 5, 100, 50, SSD1306_WHITE);
   pantalla.drawCircle(55,25,20,SSD1306_WHITE);
   pantalla.drawRoundRect(10,5,30,25,5,SSD1306_WHITE);
